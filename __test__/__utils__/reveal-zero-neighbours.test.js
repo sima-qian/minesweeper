8a0e93528b91401ee8acba785ed1d3de4d1cadaf
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-10-15 23:08:42 +0100

    convert revealZeroNeighbours() to pure funtion, Relates #13

--- __test__/__utils__/reveal-zero-neighbours.test.js
+++ __test__/__utils__/reveal-zero-neighbours.test.js
@@ -6,11 +6,63 @@ import data from "./utils-data";
 describe("Testing revealZeroNeighbours()", () => {
   const tileId = 11;
   test("revealZeroNeighbours() should return array of same length given", () => {
-    expect(revealZeroNeighbours(data.board, tileId)).toBeInstanceOf(Array);
-    expect(revealZeroNeighbours(data.board, tileId).length).toBe(
-      data.width * data.height
+    expect(
+      revealZeroNeighbours(tileId, data.board, data.width, data.height)
+    ).toBeInstanceOf(Array);
+    expect(
+      revealZeroNeighbours(tileId, data.board, data.width, data.height).length
+    ).toBe(data.width * data.height);
+  });
+  test("revealZeroNeighbours should return fully displayed board given uninitialised board", () => {
+    revealZeroNeighbours(tileId, data.board, data.width, data.height).forEach(
+      tile => {
+        expect(tile.displayed).toBeTruthy();
+        expect(tile.marked).toBeFalsy();
+      }
     );
   });
 
-  // more tests
+  const smallTestBoard = [
+    { value: 2, displayed: false, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 1, displayed: false, marked: false },
+    { value: 0, displayed: false, marked: false },
+    { value: 0, displayed: false, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 1, displayed: false, marked: false },
+    { value: 0, displayed: false, marked: false },
+    { value: 0, displayed: false, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 1, displayed: false, marked: false },
+    { value: 0, displayed: false, marked: false },
+    { value: 0, displayed: false, marked: false }
+  ];
+
+  const smallTestBoardResult = [
+    { value: 2, displayed: false, marked: false },
+    { value: 2, displayed: true, marked: false },
+    { value: 2, displayed: true, marked: false },
+    { value: 2, displayed: true, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 1, displayed: true, marked: false },
+    { value: 0, displayed: true, marked: false },
+    { value: 0, displayed: true, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 1, displayed: true, marked: false },
+    { value: 0, displayed: true, marked: false },
+    { value: 0, displayed: true, marked: false },
+    { value: 2, displayed: false, marked: false },
+    { value: 1, displayed: true, marked: false },
+    { value: 0, displayed: true, marked: false },
+    { value: 0, displayed: true, marked: false }
+  ];
+
+  test("revealZeroNeighbours should return correct result of test board", () => {
+    expect(revealZeroNeighbours(15, smallTestBoard, 4, 4)).toEqual(
+      smallTestBoardResult
+    );
+  });
 });
--- utils/reveal-zero-neighbours.js
+++ utils/reveal-zero-neighbours.js
@@ -1,6 +1,19 @@
-const revealZeroNeighbours = () => {
-  // write function
-  return false;
+import getSurroundingTiles from "./get-surrounding-tiles";
+
+const revealZeroNeighbours = (tile, board, width, height) => {
+  const surroundingTiles = getSurroundingTiles(tile, width);
+  surroundingTiles.forEach(surroundingTile => {
+    if (surroundingTile >= 0 && surroundingTile < width * height) {
+      if (!board[surroundingTile].displayed) {
+        board[surroundingTile].displayed = true;
+        board[surroundingTile].marked = false;
+        if (board[surroundingTile].value == 0) {
+          revealZeroNeighbours(surroundingTile, board, width, height);
+        }
+      }
+    }
+  });
+  return board;
 };
 
 export default revealZeroNeighbours;
